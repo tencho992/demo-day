96ab39ed1fff2388dc6cde691dc3574e7267084b
Tenzin Chosang <tenzinchosang.dev@gmail.com>
2023-04-30 22:59:59 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://tenzinc:tenzinc@burn-notice.ofi7dnh.mongodb.net/demoday?retryWrites=true&w=majority', 
+    'dbName': 'demoday'
+};
